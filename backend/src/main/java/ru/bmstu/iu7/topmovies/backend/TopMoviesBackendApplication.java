2e7ca7a26ff1aa6c36fbe552e7c8508d4b90e10a
denis96z <zinoviev.denis@yandex.ru>
2019-02-22 14:33:29 +0300

    backend: initial commit

---
+++ backend/src/main/java/ru/bmstu/iu7/topmovies/backend/TopMoviesBackendApplication.java
@@ -0,0 +1,11 @@
+package ru.bmstu.iu7.topmovies.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopMoviesBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TopMoviesBackendApplication.class, args);
+    }
+}
