9bdc5faecef3dba997a610be2a1eaa3cce25d1b4
denis96z <zinoviev.denis@yandex.ru>
2019-02-23 23:43:46 +0300

    basic html

---
+++ frontend/src/app/movie.ts
@@ -0,0 +1,10 @@
+export class Movie {
+  id: string;
+  title: string;
+  year: number;
+  genre: string;
+  director: string;
+  actors: Array<string>;
+  rating: number;
+  description: string;
+}
