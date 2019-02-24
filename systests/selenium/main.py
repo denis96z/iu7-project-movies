2a99c153110209df4550c084631e25a801a538ad
denis96z <zinoviev.denis@yandex.ru>
2019-02-24 03:42:01 +0300

    sys test added

--- systests/selenium/main.py
+++ systests/selenium/main.py
@@ -3,5 +3,12 @@ from selenium import webdriver
 
 driver = webdriver.Firefox()
 driver.get("http://localhost:8080")
-print(driver.title)
+
+btn = driver.find_element_by_tag_name('button')
+btn.click()
+
+movies = driver.find_elements_by_tag_name('a')
+for m in movies:
+    m.click()
+
 driver.close()
